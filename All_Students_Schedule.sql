52bbc1472566a33dc34d51e643cdfbfabfbe25ea
austindevesholmes <51881192+austindevesholmes@users.noreply.github.com>
2020-08-12 14:32:02 -0400

    Update All_Students_Schedule.sql

--- All_Students_Schedule.sql
+++ All_Students_Schedule.sql
@@ -17,20 +17,20 @@ it.lastname as TeacherLastName,
 it.firstname as TeacherFirstName, 
 ctt.email as TeacherEmail 
 
-FROM  person p
-join [identity] i on i.identityID = p.currentidentityID
-join enrollment e on e.personID = p.personID
-join calendar cl on cl.calendarID = e.calendarID 
+FROM  prsn p
+join [iden] i on i.identityID = p.currentidentityID
+join enroll e on e.personID = p.personID
+join calen cl on cl.calendarID = e.calendarID 
 join school sl ON sl.schoolid = cl.schoolid 
 join schoolyear sy on sy.endyear = e.endyear and sy.active =1
 left join contact ct on ct.personID = p.personID
 join view_schedules_k12 sc on sc.personID = e.personID and e.calendarID  = sc.calendarID
-left join Roster r on r.personID = p.personID
-left join Section se on se.sectionID = r.sectionID
-left join period pd on pd.periodid = se.sectionid 
-left join Course c on c.courseID = se.courseID and c.calendarID = cl.calendarID
-join person pt on pt.personID = sc.teacherPersonID
-join [identity] it on it.identityID= pt.currentIdentityID
+left join Rost r on r.personID = p.personID
+left join Sec se on se.sectionID = r.sectionID
+left join prd pd on pd.periodid = se.sectionid 
+left join Crse c on c.courseID = se.courseID and c.calendarID = cl.calendarID
+join prsn pt on pt.personID = sc.teacherPersonID
+join [iden] it on it.identityID= pt.currentIdentityID
 left join contact ctt on ctt.personID = pt.personID
 
 WHERE
