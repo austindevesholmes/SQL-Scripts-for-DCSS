048b425f09c5fbba40f5f879e4ee10786c2fc753
austindevesholmes <51881192+austindevesholmes@users.noreply.github.com>
2021-06-11 11:58:08 -0400

    Update SummerSchoolStudents.sql

--- SummerSchoolStudents.sql
+++ SummerSchoolStudents.sql
@@ -1,8 +1,8 @@
 --This query brings back all students attending summer school this semester with a start date of 06-02-2021.
 SELECT  * 
 
-FROM view_schedules_k12 sv
-JOIN student s on s.calendarid = sv.calendarid 
+FROM view_schedules sv
+JOIN stud s on s.calendarid = sv.calendarid 
 AND s.personid = sv.personid  
 
 WHERE [start_date] = '2021-06-02'
