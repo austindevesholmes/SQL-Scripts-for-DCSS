22aebdb928a53123469411c0947981ac4e1d454e
austindevesholmes <51881192+austindevesholmes@users.noreply.github.com>
2021-06-11 14:46:21 -0400

    Update Count Failing Grades.sql

--- Count Failing Grades.sql
+++ Count Failing Grades.sql
@@ -8,13 +8,13 @@ s.middlename AS 'MiddleName',
 s.grade As 'Student Grade Level',
 COUNT(s.studentnumber) AS 'Total Number of Failing Grades'
 
-FROM student s WITH(NOEXPAND)
-JOIN gradingscore gc ON gc.personid = s.personid and gc.calendarID = s.calendarID
-JOIN section sec ON sec.sectionID = gc.sectionID
-JOIN course crs ON crs.courseID = sec.courseID
-JOIN term t ON t.termID = gc.termID
-JOIN gradingTask gt ON gt.taskID = gc.taskID
-JOIN school sl ON sl.schoolid = s.schoolid
+FROM s WITH(NOEXPAND)
+JOIN gc ON gc.personid = s.personid and gc.calendarID = s.calendarID
+JOIN sec ON sec.sectionID = gc.sectionID
+JOIN crs ON crs.courseID = sec.courseID
+JOIN t ON t.termID = gc.termID
+JOIN gt ON gt.taskID = gc.taskID
+JOIN sl ON sl.schoolid = s.schoolid
 
 WHERE
 gc.score BETWEEN '0' and '69' AND gc.score <> '100'
