e7d9b849f654b73d5f969cbdc82bc00be5b9f010
austindevesholmes <51881192+austindevesholmes@users.noreply.github.com>
2021-07-22 11:10:16 -0400

    Update All_Students_Schedule.sql

--- All_Students_Schedule.sql
+++ All_Students_Schedule.sql
@@ -22,16 +22,16 @@ join [iden] i on i.identityID = p.currentidentityID
 join enroll e on e.personID = p.personID
 join calen cl on cl.calendarID = e.calendarID 
 join school sl ON sl.schoolid = cl.schoolid 
-join schoolyear sy on sy.endyear = e.endyear and sy.active =1
-left join contact ct on ct.personID = p.personID
-join view_schedules_k12 sc on sc.personID = e.personID and e.calendarID  = sc.calendarID
-left join Rost r on r.personID = p.personID
-left join Sec se on se.sectionID = r.sectionID
+join syear sy on sy.endyear = e.endyear and sy.active =1
+left join cont ct on ct.personID = p.personID
+join view_k12 sc on sc.personID = e.personID and e.calendarID  = sc.calendarID
+left join rost r on r.personID = p.personID
+left join sec se on se.sectionID = r.sectionID
 left join prd pd on pd.periodid = se.sectionid 
 left join Crse c on c.courseID = se.courseID and c.calendarID = cl.calendarID
 join prsn pt on pt.personID = sc.teacherPersonID
 join [iden] it on it.identityID= pt.currentIdentityID
-left join contact ctt on ctt.personID = pt.personID
+left join cont ctt on ctt.personID = pt.personID
 
 WHERE
 sy.active = 1 
